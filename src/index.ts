8f53b142f8e6f98680aabd7a928c40a0751a3a2b
Rohit Tiwari <tigertiwari1023@gmail.com>
2025-09-29 18:17:21 +0530

    fix: env issue

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    name: "session",
+    name: "session-pms",
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
     secure: true,
